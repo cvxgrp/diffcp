fdb660a376339aadc3da28945fc69851f7561717
Shane Barratt <stbarratt@gmail.com>
2019-10-04 12:33:20 -0700

    added pybind wrapper and modified setup file'

--- cpp/src/deriv.cpp
+++ cpp/src/deriv.cpp
@@ -1,7 +1,7 @@
 #include "deriv.h"
 #include "lsqr.h"
 
-Vector _solve_derivative(SparseMatrix Q, ListOfCones cones, Vector z, Vector rhs) {
+Vector _solve_derivative(const SparseMatrix& Q, const ListOfCones cones, const Vector& z, const Vector& rhs) {
   int N = z.size();
 
   LinearOperator M = (Q - eye(N)) * dpi(z, cones) + eye(N);
@@ -9,7 +9,7 @@ Vector _solve_derivative(SparseMatrix Q, ListOfCones cones, Vector z, Vector rhs
   return result.x;
 }
 
-Vector _solve_adjoint_derivative(SparseMatrix Q, ListOfCones cones, Vector z, Vector dz) {
+Vector _solve_adjoint_derivative(const SparseMatrix& Q, const ListOfCones cones, const Vector& z, const Vector& dz) {
   int N = z.size();
 
   LinearOperator MT = dpi(z, cones).transpose() * (Q.transpose() - eye(N)) + eye(N);
