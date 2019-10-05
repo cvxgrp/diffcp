ea437de3b4c67b8de254efb98ba43bcd8c9cf4df
Akshay <akshaykagrawal7@gmail.com>
2019-10-04 17:07:22 -0700

    initialize t

--- cpp/src/cones.cpp
+++ cpp/src/cones.cpp
@@ -189,7 +189,7 @@ LinearOperator _dprojection_exp(const Vector &x, bool dual) {
       };
       ops.emplace_back(LinearOperator(3, 3, matvec, matvec));
     } else {
-      double t;
+      double t = 0;
       double rs[3] = {0.0, 0.0, 0.0};
       assert(_proj_exp_cone(rs, &t) == 0);
       double r = rs[0];
