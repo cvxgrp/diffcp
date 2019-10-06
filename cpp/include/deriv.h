f314a9f95479cb979ca8f4abe3e5b64b626e10c9
Brandon Amos <brandon.amos.cs@gmail.com>
2019-10-06 15:01:13 -0700

    remove accidentally-committed bit

--- cpp/include/deriv.h
+++ cpp/include/deriv.h
@@ -17,7 +17,5 @@ Vector _solve_adjoint_derivative_dense(const Matrix& MT, const Vector& dz);
 // TODO: These should also have (at least) rho/it_ref_iters and MT_iref
 SparseMatrix M_sparse(const SparseMatrix& Q, const std::vector<Cone>& cones,
     const Vector& u, const Vector& v, double w);
-            rho = kwargs.get("rho", 1e-6)
-            it_ref_iters = kwargs.get("it_ref_iters", 5)
 Vector _solve_derivative_sparse(const SparseMatrix& M, const Vector& rhs);
 Vector _solve_adjoint_derivative_sparse(const SparseMatrix& MT, const Vector& dz);
