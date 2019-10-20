5f9a6a4cf5fdf49285202adb318cf77e0490d4c3
Shane Barratt <stbarratt@gmail.com>
2019-10-20 15:15:30 -0700

    fixed pybind11 error (#20)

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools.command.build_ext import build_ext
 import sys
 import setuptools
 from glob import glob
+import subprocess
 
 
 with open("README.md", "r") as fh:
@@ -18,6 +19,11 @@ class get_pybind_include(object):
     method can be invoked. """
 
     def __init__(self, user=False):
+        try:
+            import pybind11
+        except ImportError:
+            if subprocess.call([sys.executable, '-m', 'pip', 'install', 'pybind11']):
+                raise RuntimeError('pybind11 install failed.')
         self.user = user
 
     def __str__(self):
