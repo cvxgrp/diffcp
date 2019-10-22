8335334494424591ea8ab91e266719923c7ab378
Akshay <akshaykagrawal7@gmail.com>
2019-10-21 23:05:32 -0700

    formatting

--- diffcp/__init__.py
+++ diffcp/__init__.py
@@ -1,5 +1,7 @@
 __version__ = "1.0.7"
 
-from diffcp.cone_program import solve_and_derivative, solve_and_derivative_batch, solve_and_derivative_internal, SolverError
+from diffcp.cone_program import solve_and_derivative, \
+                                solve_and_derivative_batch, \
+                                solve_and_derivative_internal, SolverError
 from diffcp.cones import ZERO, POS, SOC, PSD, EXP
 from diffcp import utils
